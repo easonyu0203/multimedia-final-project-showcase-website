92ba1fb8d539a200c5ca82fdc1e5c187208073d7
easonyu0203 <breaking0203@gmail.com>
2023-09-14 01:10:36 +1000

    feat: init

---
+++ src/hiphop.ts
@@ -0,0 +1,101 @@
+import type { CustomThemeConfig } from "@skeletonlabs/tw-plugin";
+
+export const hiphop: CustomThemeConfig = {
+  name: "hiphop",
+  properties: {
+    // =~= Theme Properties =~=
+    "--theme-font-family-base": `system-ui`,
+    "--theme-font-family-heading": `system-ui`,
+    "--theme-font-color-base": "0 0 0",
+    "--theme-font-color-dark": "255 255 255",
+    "--theme-rounded-base": "9999px",
+    "--theme-rounded-container": "8px",
+    "--theme-border-base": "1px",
+    // =~= Theme On-X Colors =~=
+    "--on-primary": "0 0 0",
+    "--on-secondary": "0 0 0",
+    "--on-tertiary": "0 0 0",
+    "--on-success": "0 0 0",
+    "--on-warning": "0 0 0",
+    "--on-error": "255 255 255",
+    "--on-surface": "0 0 0",
+    // =~= Theme Colors  =~=
+    // primary | #FF6D00
+    "--color-primary-50": "255 233 217", // #ffe9d9
+    "--color-primary-100": "255 226 204", // #ffe2cc
+    "--color-primary-200": "255 219 191", // #ffdbbf
+    "--color-primary-300": "255 197 153", // #ffc599
+    "--color-primary-400": "255 153 77", // #ff994d
+    "--color-primary-500": "255 109 0", // #FF6D00
+    "--color-primary-600": "230 98 0", // #e66200
+    "--color-primary-700": "191 82 0", // #bf5200
+    "--color-primary-800": "153 65 0", // #994100
+    "--color-primary-900": "125 53 0", // #7d3500
+    // secondary | #D94F15
+    "--color-secondary-50": "249 229 220", // #f9e5dc
+    "--color-secondary-100": "247 220 208", // #f7dcd0
+    "--color-secondary-200": "246 211 197", // #f6d3c5
+    "--color-secondary-300": "240 185 161", // #f0b9a1
+    "--color-secondary-400": "228 132 91", // #e4845b
+    "--color-secondary-500": "217 79 21", // #D94F15
+    "--color-secondary-600": "195 71 19", // #c34713
+    "--color-secondary-700": "163 59 16", // #a33b10
+    "--color-secondary-800": "130 47 13", // #822f0d
+    "--color-secondary-900": "106 39 10", // #6a270a
+    // tertiary | #FFE3C4
+    "--color-tertiary-50": "255 251 246", // #fffbf6
+    "--color-tertiary-100": "255 249 243", // #fff9f3
+    "--color-tertiary-200": "255 248 240", // #fff8f0
+    "--color-tertiary-300": "255 244 231", // #fff4e7
+    "--color-tertiary-400": "255 235 214", // #ffebd6
+    "--color-tertiary-500": "255 227 196", // #FFE3C4
+    "--color-tertiary-600": "230 204 176", // #e6ccb0
+    "--color-tertiary-700": "191 170 147", // #bfaa93
+    "--color-tertiary-800": "153 136 118", // #998876
+    "--color-tertiary-900": "125 111 96", // #7d6f60
+    // success | #78C055
+    "--color-success-50": "235 246 230", // #ebf6e6
+    "--color-success-100": "228 242 221", // #e4f2dd
+    "--color-success-200": "221 239 213", // #ddefd5
+    "--color-success-300": "201 230 187", // #c9e6bb
+    "--color-success-400": "161 211 136", // #a1d388
+    "--color-success-500": "120 192 85", // #78C055
+    "--color-success-600": "108 173 77", // #6cad4d
+    "--color-success-700": "90 144 64", // #5a9040
+    "--color-success-800": "72 115 51", // #487333
+    "--color-success-900": "59 94 42", // #3b5e2a
+    // warning | #FFC107
+    "--color-warning-50": "255 246 218", // #fff6da
+    "--color-warning-100": "255 243 205", // #fff3cd
+    "--color-warning-200": "255 240 193", // #fff0c1
+    "--color-warning-300": "255 230 156", // #ffe69c
+    "--color-warning-400": "255 212 81", // #ffd451
+    "--color-warning-500": "255 193 7", // #FFC107
+    "--color-warning-600": "230 174 6", // #e6ae06
+    "--color-warning-700": "191 145 5", // #bf9105
+    "--color-warning-800": "153 116 4", // #997404
+    "--color-warning-900": "125 95 3", // #7d5f03
+    // error | #D43F00
+    "--color-error-50": "249 226 217", // #f9e2d9
+    "--color-error-100": "246 217 204", // #f6d9cc
+    "--color-error-200": "244 207 191", // #f4cfbf
+    "--color-error-300": "238 178 153", // #eeb299
+    "--color-error-400": "225 121 77", // #e1794d
+    "--color-error-500": "212 63 0", // #D43F00
+    "--color-error-600": "191 57 0", // #bf3900
+    "--color-error-700": "159 47 0", // #9f2f00
+    "--color-error-800": "127 38 0", // #7f2600
+    "--color-error-900": "104 31 0", // #681f00
+    // surface | #FF6D00
+    "--color-surface-50": "255 233 217", // #ffe9d9
+    "--color-surface-100": "255 226 204", // #ffe2cc
+    "--color-surface-200": "255 219 191", // #ffdbbf
+    "--color-surface-300": "255 197 153", // #ffc599
+    "--color-surface-400": "255 153 77", // #ff994d
+    "--color-surface-500": "255 109 0", // #FF6D00
+    "--color-surface-600": "230 98 0", // #e66200
+    "--color-surface-700": "191 82 0", // #bf5200
+    "--color-surface-800": "153 65 0", // #994100
+    "--color-surface-900": "125 53 0", // #7d3500
+  },
+};
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,22 @@
 import adapter from "@sveltejs/adapter-static";
+
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  extensions: [".svelte"],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess()],
 
   kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: "/~yuyu0984",
+      base: process.env.NODE_ENV === "production" ? "/~yuyu0984" : "",
     },
   },
 };
-
 export default config;
